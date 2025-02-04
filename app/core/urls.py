b2bd921cd51f1500f525ae49cd982cc88010c55b
WailBenoulha <wailbnlh@gmail.com>
2025-02-04 02:51:00 -1100

    add theatre and movie

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ActorView
+from .views import ActorView,MovieView
 
 urlpatterns = [
     path('actors',ActorView.as_view(),name='actors'),
-    path('actors/<int:pk>/',ActorView.as_view(),name='actors')
+    path('actors/<int:pk>/',ActorView.as_view(),name='actor'),
+    path('movies',MovieView.as_view(),name='movies'),
+    path('movies/<int:pk>/',MovieView.as_view(),name='movie')
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -48,3 +48,93 @@ class ActorView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)    
         actor.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class MovieView(APIView):
+    serializer_class = MovieSerializer
+    permission_classes = [AllowAny]
+
+    
+    def get(self,request,pk=None):
+        if pk:
+            try:
+                movie = Movie.objects.get(pk=pk) 
+            except Movie.DoesNotExist:   
+                return Response(status=status.HTTP_404_NOT_FOUND)  
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        movie = Movie.objects.all()  
+        serializer = ActorSerializer(movie,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self,request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"New Movie Created!"},status=status.HTTP_201_CREATED)    
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request,pk):
+        try:
+           movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)   
+        serializer = MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk=None):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)    
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)    
+    
+    
+class TheatreView(APIView):
+    serializer_class = TheatreSerializer
+    permission_classes = [AllowAny]
+
+    
+    def get(self,request,pk=None):
+        if pk:
+            try:
+                theatre = Theatre.objects.get(pk=pk) 
+            except Theatre.DoesNotExist:   
+                return Response(status=status.HTTP_404_NOT_FOUND)  
+            serializer = TheatreSerializer(theatre)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        theatre = Theatre.objects.all()  
+        serializer = ActorSerializer(theatre,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self,request):
+        serializer = TheatreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"New Theatre Created!"},status=status.HTTP_201_CREATED)    
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request,pk):
+        try:
+           theatre = Theatre.objects.get(pk=pk)
+        except Theatre.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)   
+        serializer = TheatreSerializer(theatre,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk=None):
+        try:
+            theatre = Theatre.objects.get(pk=pk)
+        except Theatre.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)    
+        theatre.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  
+
+
