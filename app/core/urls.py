16e1f63146be156e9a36fff4e99d844633c3d38c
WailBenoulha <wailbnlh@gmail.com>
2025-02-04 03:22:03 -1100

    update with generics

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ActorView,MovieView,TheatreView
+from .views import ActorListView,ActorDetailView,MovieListView,MovieDetailView,TheatreView
 
 urlpatterns = [
-    path('actors',ActorView.as_view(),name='actors'),
-    path('actors/<int:pk>/',ActorView.as_view(),name='actor'),
-    path('movies',MovieView.as_view(),name='movies'),
-    path('movies/<int:pk>/',MovieView.as_view(),name='movie'),
+    path('actors',ActorListView.as_view(),name='actors'),
+    path('actors/<int:pk>/',ActorDetailView.as_view(),name='actor'),
+    path('movies',MovieListView.as_view(),name='movies'),
+    path('movies/<int:pk>/',MovieDetailView.as_view(),name='movie'),
     path('theatres',TheatreView.as_view(),name='theatres'),
     path('theatres/<int:pk>/',TheatreView.as_view(),name='theatre')
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from .serializers import *
 from .models import *
 from rest_framework.response import Response
@@ -7,93 +8,25 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 
 
-class ActorView(APIView):
+class ActorListView(ListCreateAPIView):
+    queryset = Actors.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = [AllowAny]
+    
+class ActorDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Actors.objects.all()
     serializer_class = ActorSerializer
     permission_classes = [AllowAny]
 
-    
-    def get(self,request,pk=None):
-        if pk:
-            try:
-                actor = Actors.objects.get(pk=pk) 
-            except Actors.DoesNotExist:   
-                return Response(status=status.HTTP_404_NOT_FOUND)  
-            serializer = ActorSerializer(actor)
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        actor = Actors.objects.all()  
-        serializer = ActorSerializer(actor,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    def post(self,request):
-        serializer = ActorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message":"New Actor Created!"},status=status.HTTP_201_CREATED)    
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self,request,pk):
-        try:
-           actor = Actors.objects.get(pk=pk)
-        except Actors.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)   
-        serializer = ActorSerializer(actor,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self,request,pk=None):
-        try:
-            actor = Actors.objects.get(pk=pk)
-        except Actors.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)    
-        actor.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
-class MovieView(APIView):
+class MovieListView(ListCreateAPIView):
+    queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny]   
 
-    
-    def get(self,request,pk=None):
-        if pk:
-            try:
-                movie = Movie.objects.get(pk=pk) 
-            except Movie.DoesNotExist:   
-                return Response(status=status.HTTP_404_NOT_FOUND)  
-            serializer = MovieSerializer(movie)
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        movie = Movie.objects.all()  
-        serializer = MovieSerializer(movie,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    def post(self,request):
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message":"New Movie Created!"},status=status.HTTP_201_CREATED)    
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self,request,pk):
-        try:
-           movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)   
-        serializer = MovieSerializer(movie,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self,request,pk=None):
-        try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)    
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)    
-    
+class MovieDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [AllowAny]       
 
 class TheatreView(APIView):
     serializer_class = TheatreSerializer
